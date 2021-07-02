22e2f5c32336b17e0257eab5a01230a343d321c8
Louise <louisesoulier789@gmail.com>
2021-07-02 12:11:58 +0200

    cette fois c'est la bonne

--- mini_serv.c
+++ mini_serv.c
@@ -6,6 +6,7 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#define BUFFER_SIZE 1
 
 typedef struct	s_list {
 	int fd;
@@ -16,7 +17,8 @@ typedef struct	s_list {
 t_list *head = NULL;
 int server_fd, g_id = 0;
 fd_set curr_sock, cpy_read, cpy_write;
-char msg[42], str[42*4096], tmp[42*4096], buf[42*4096 + 42];
+char buffer[BUFFER_SIZE + 1], msg[42];
+char *str_joined, *for_extract, *for_sprintf;
 
 void write_error(char *error_str) {
 	char *to_write = error_str ? error_str : "Fatal error\n";
@@ -117,11 +119,12 @@ int setup_server(int port) {
 	return (fd);
 }
 
-int accept_client() {
+void accept_client() {
 	int client_fd;
 	struct sockaddr_in c_addr;
 	socklen_t len = sizeof(c_addr);
 
+	bzero(&msg, sizeof(msg));
 	client_fd = accept(server_fd, (struct sockaddr*)&c_addr, &len);
 	if (client_fd < 0)
 		write_error(NULL);
@@ -130,23 +133,107 @@ int accept_client() {
 	FD_SET(client_fd, &curr_sock);
 }
 
-int ex_msg(int fd) {
-	int i = 0;
-	int j = 0;
-	
-	while(str[i]) {
-		tmp[j] = str[i];
-		j++;
-		if (str[i] == '\n') {
-			sprintf(buf, "client %d: %s", get_id(fd), tmp);
-			send_all(fd, buf);
-			j = 0;
-			bzero(&tmp, strlen(tmp));
-			bzero(&buf, strlen(buf));
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
 		}
 		i++;
 	}
-	bzero(&str, strlen(str));
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+void client_leave(int fd) {
+	bzero(&msg, sizeof(msg));
+	sprintf(msg, "server: client %d just left\n", rm_client(fd));
+	send_all(fd, msg);
+	FD_CLR(fd, &curr_sock);
+	close(fd);
+}
+
+void client_receive(int fd) {
+	for_sprintf = (char*)calloc(42 + strlen(for_extract), sizeof(char));
+	if (!for_sprintf)
+		write_error(NULL);
+	sprintf(for_sprintf, "client %d: %s", get_id(fd), for_extract);
+	free(for_extract);
+	send_all(fd, for_sprintf);
+	free(for_sprintf);
+}
+
+void select_loop(int max_fd) {
+	int ret;
+
+	for(int fd = 0; fd < max_fd + 1; fd++) {	
+		if (FD_ISSET(fd, &cpy_read)) {
+			if (fd == server_fd) {
+				accept_client();
+				break;
+			} else {
+				if ((ret = recv(fd, buffer, BUFFER_SIZE, 0)) <= 0) {
+					client_leave(fd);
+					break;
+				} else {
+					buffer[ret] = '\0';
+					str_joined = str_join(str_joined, buffer);
+					if (extract_message(&str_joined, &for_extract) == 1) {
+						client_receive(fd);	
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+void init_server(int port) {
+	server_fd = setup_server(port);
+	if (server_fd == -1)
+		write_error(NULL);
+
+	FD_ZERO(&curr_sock);
+	FD_SET(server_fd, &curr_sock);
+	bzero(buffer, BUFFER_SIZE + 1);
+	str_joined = (char*)calloc(1, sizeof(char));
+	if (!str_joined)
+		write_error(NULL);
 }
 
 int main (int argc, char **argv) {
@@ -154,44 +241,15 @@ int main (int argc, char **argv) {
 	
 	if (argc != 2)
 		write_error("Wrong number of arguments\n");
-	server_fd = setup_server(atoi(argv[1]));
-	if (server_fd == -1)
-		write_error(NULL);
-
-	FD_ZERO(&curr_sock);
-	FD_SET(server_fd, &curr_sock);
-	bzero(&tmp, sizeof(tmp));
-	bzero(&buf, sizeof(buf));
-	bzero(&str, sizeof(str));
-
+	init_server(atoi(argv[1]));
 	while(1) {
 		cpy_write = cpy_read = curr_sock;
 		max_fd = get_max_fd();
 		if (select(max_fd + 1, &cpy_read, &cpy_write, NULL, NULL) < 0)
 			continue;
-		for(int fd = 0; fd < max_fd + 1; fd++) {
-			if (FD_ISSET(fd, &cpy_read)) {
-				if (fd == server_fd) {
-					bzero(&msg, sizeof(msg));
-					accept_client();
-					break;
-				}
-				else {
-					if (recv(fd, str, sizeof(str), 0) <= 0) {
-						bzero(&msg, sizeof(msg));
-						sprintf(msg, "server: client %d just left\n", rm_client(fd));
-						send_all(fd, msg);
-						FD_CLR(fd, &curr_sock);
-						close(fd);
-						break;
-					} else {
-						ex_msg(fd);
-					}
-				}
-			}
-		}
+		select_loop(max_fd);
 	}
+	free(str_joined);
 	return (0);
 }
-	
 
